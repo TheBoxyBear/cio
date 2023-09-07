ede0b6ba6aafe3c09855a848abbdb85c47dd5270
TheBoxyBear <15822255+TheBoxyBear@users.noreply.github.com>
2023-09-07 11:02:21 -0400

    Fix header signatures

--- write.h
+++ write.h
@@ -3,8 +3,8 @@
 void WriteC(char c);
 void WriteCLine(char c);
 
-void Write(char*);
-void WriteLine(char* str);
+void Write(const char*);
+void WriteLine(const char* str);
 
 char* i_to_string(int n);
 char* f_to_string(float n);
