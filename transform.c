bd2c7a1636ba9fc790d88a653fa14fcf6460dff1
TheBoxyBear <15822255+TheBoxyBear@users.noreply.github.com>
2023-09-15 09:39:36 -0400

    Update transform.c

--- transform.c
+++ transform.c
@@ -67,18 +67,6 @@ char* Reverse(const char* str)
     return out;
 }
 
-/// @brief Swaps the values in two pointers.
-/// @param a
-/// @param b
-void Swap(void* a, void* b)
-{
-    *a = *b;
-
-    void* tmp = a;
-    a = b;
-    b = tmp;
-}
-
 /// @brief Sorts an array of bytes.
 /// @param arr
 /// @param len
